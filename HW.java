49f9f4f9459137487d3651fa3ebe534bae75a6f0
Yan Silberg <yansilberg08@gmail.com>
2024-01-05 07:38:42 +0200

    My commit

---
+++ HW.java
@@ -0,0 +1,15 @@
+public class HW {
+  public int returnInt() {
+    int number = 4;
+    return number;
+  }
+
+  public void returnVoid() {
+    return;
+  }
+
+  public boolean returnBool() {
+    boolean bool = true;
+    return bool;
+  }
+}
